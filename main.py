70beff9ff9832e2a09d18e9fac61c41b9965e859
Mohamed <mohamedkhalil.e@gmail.comm>
2022-11-17 01:54:21 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-'}
+
+welcome_msg = "\n/////////// Welcome to the text to Morse converter ///////////"
+
+
+def morse_code_trans(text):
+    translated_msg = ""
+    for c in text:
+        if c in MORSE_CODE_DICT:
+            translated_msg += MORSE_CODE_DICT[c]
+        else:
+            translated_msg += c
+    return print(f"your Morse Translated message is:\n\n{translated_msg}")
+
+
+text = (input(f'{welcome_msg}\nPlease write down the message you want to convert?\n')).upper()
+morse_code_trans(text)
